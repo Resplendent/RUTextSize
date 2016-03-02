4b164c6051f95f87ac5538067343aa7974122e99
Benjamin Maer <ben@resplendent.co>
2016-03-02 18:32:55 -0500

    Added more example project code.

--- Example/RUTextSize/Code/UIViewController/RUViewController.m
+++ Example/RUTextSize/Code/UIViewController/RUViewController.m
@@ -11,6 +11,7 @@
 #import "RUTextViewWithPlaceholderContainerView.h"
 
 #import <ResplendentUtilities/NSString+RUMacros.h>
+#import <ResplendentUtilities/RUConditionalReturn.h>
 
 #import <RTSMTableSectionManager/RTSMTableSectionManager.h>
 
@@ -19,10 +20,13 @@
 
 
 typedef NS_ENUM(NSInteger, RUViewController__tableView_section) {
-    RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView,
+	RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView,
+	RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView_textCenteredHorizontally,
+	RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView_textCenteredVertically,
+	RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView_textCenteredHorizontallyAndVertically,
 
 	RUViewController__tableView_section__first		= RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView,
-	RUViewController__tableView_section__last		= RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView,
+	RUViewController__tableView_section__last		= RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView_textCenteredHorizontallyAndVertically,
 };
 
 
@@ -47,6 +51,12 @@ typedef NS_ENUM(NSInteger, RUViewController__tableView_section) {
 
 #pragma mark - genericTableViewCell_CustomView Custom Views
 -(nonnull RUTextViewWithPlaceholderContainerView*)genericTableViewCell_CustomView_RUTextViewWithPlaceholderContainerView;
+-(void)apply_textCenteredHorizontally_toRUTextViewWithPlaceholderContainerView:(nonnull RUTextViewWithPlaceholderContainerView*)textViewWithPlaceholderContainerView;
+-(void)apply_textCenteredVertically_toRUTextViewWithPlaceholderContainerView:(nonnull RUTextViewWithPlaceholderContainerView*)textViewWithPlaceholderContainerView;
+
+-(nonnull RUTextViewWithPlaceholderContainerView*)genericTableViewCell_CustomView_RUTextViewWithPlaceholderContainerView_textCenteredHorizontally;
+-(nonnull RUTextViewWithPlaceholderContainerView*)genericTableViewCell_CustomView_RUTextViewWithPlaceholderContainerView_textCenteredVertically;
+-(nonnull RUTextViewWithPlaceholderContainerView*)genericTableViewCell_CustomView_RUTextViewWithPlaceholderContainerView_textCenteredHorizontallyAndVertically;
 
 @end
 
@@ -116,6 +126,9 @@ typedef NS_ENUM(NSInteger, RUViewController__tableView_section) {
 	switch (tableSection)
 	{
 		case RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView:
+		case RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView_textCenteredHorizontally:
+		case RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView_textCenteredVertically:
+		case RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView_textCenteredHorizontallyAndVertically:
 			return 60.0f;
 			break;
 	}
@@ -132,6 +145,12 @@ typedef NS_ENUM(NSInteger, RUViewController__tableView_section) {
 		case RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView:
 			return 20.0f;
 			break;
+
+		case RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView_textCenteredHorizontally:
+		case RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView_textCenteredVertically:
+		case RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView_textCenteredHorizontallyAndVertically:
+			return 30.0f;
+			break;
 	}
 	
 	NSAssert(false, @"unhandled tableSection %li",(long)tableSection);
@@ -145,6 +164,7 @@ typedef NS_ENUM(NSInteger, RUViewController__tableView_section) {
 	[headerLabel setTextAlignment:NSTextAlignmentCenter];
 	[headerLabel setFont:[UIFont systemFontOfSize:12.0f weight:UIFontWeightMedium]];
 	[headerLabel setTextColor:[UIColor darkTextColor]];
+	[headerLabel setNumberOfLines:0];
 	[headerLabel setText:[self headerLabelTextForIndexPathSection:section]];
 
 	return headerLabel;
@@ -159,6 +179,18 @@ typedef NS_ENUM(NSInteger, RUViewController__tableView_section) {
 		case RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView:
 			return @"RUGenericTableViewCell_CustomView";
 			break;
+			
+		case RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView_textCenteredHorizontally:
+			return @"RUGenericTableViewCell_CustomView\ntext centered horizontally";
+			break;
+
+		case RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView_textCenteredVertically:
+			return @"RUGenericTableViewCell_CustomView\ntext centered vertically";
+			break;
+
+		case RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView_textCenteredHorizontallyAndVertically:
+			return @"RUGenericTableViewCell_CustomView\ntext centered horizontally and vertically";
+			break;
 	}
 	
 	NSAssert(false, @"unhandled tableSection %li",(long)tableSection);
@@ -172,6 +204,18 @@ typedef NS_ENUM(NSInteger, RUViewController__tableView_section) {
 	{
 		case RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView:
 			return [self genericTableViewCell_CustomView_withCustomView:[self genericTableViewCell_CustomView_RUTextViewWithPlaceholderContainerView]];
+			break;
+			
+		case RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView_textCenteredHorizontally:
+			return [self genericTableViewCell_CustomView_withCustomView:[self genericTableViewCell_CustomView_RUTextViewWithPlaceholderContainerView_textCenteredHorizontally]];
+			break;
+			
+		case RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView_textCenteredVertically:
+			return [self genericTableViewCell_CustomView_withCustomView:[self genericTableViewCell_CustomView_RUTextViewWithPlaceholderContainerView_textCenteredVertically]];
+			break;
+
+		case RUViewController__tableView_section_RUTextViewWithPlaceholderContainerView_textCenteredHorizontallyAndVertically:
+			return [self genericTableViewCell_CustomView_withCustomView:[self genericTableViewCell_CustomView_RUTextViewWithPlaceholderContainerView_textCenteredHorizontallyAndVertically]];
 			break;
 	}
 
@@ -201,6 +245,46 @@ typedef NS_ENUM(NSInteger, RUViewController__tableView_section) {
 {
 	RUTextViewWithPlaceholderContainerView* textViewWithPlaceholderContainerView = [RUTextViewWithPlaceholderContainerView new];
 	[textViewWithPlaceholderContainerView.textViewPlaceholderLabel setText:@"Placeholder text"];
+	return textViewWithPlaceholderContainerView;
+}
+
+-(void)apply_textCenteredHorizontally_toRUTextViewWithPlaceholderContainerView:(nonnull RUTextViewWithPlaceholderContainerView*)textViewWithPlaceholderContainerView
+{
+	kRUConditionalReturn(textViewWithPlaceholderContainerView == nil, YES);
+
+	[textViewWithPlaceholderContainerView.textView setTextAlignment:NSTextAlignmentCenter];
+	[textViewWithPlaceholderContainerView.textViewPlaceholderLabel setTextAlignment:NSTextAlignmentCenter];
+}
+
+-(void)apply_textCenteredVertically_toRUTextViewWithPlaceholderContainerView:(nonnull RUTextViewWithPlaceholderContainerView*)textViewWithPlaceholderContainerView
+{
+	kRUConditionalReturn(textViewWithPlaceholderContainerView == nil, YES);
+	
+	[textViewWithPlaceholderContainerView setCenterTextVertically:YES];
+}
+
+-(nonnull RUTextViewWithPlaceholderContainerView*)genericTableViewCell_CustomView_RUTextViewWithPlaceholderContainerView_textCenteredHorizontally
+{
+	RUTextViewWithPlaceholderContainerView* textViewWithPlaceholderContainerView = [self genericTableViewCell_CustomView_RUTextViewWithPlaceholderContainerView];
+	[self apply_textCenteredHorizontally_toRUTextViewWithPlaceholderContainerView:textViewWithPlaceholderContainerView];
+
+	return textViewWithPlaceholderContainerView;
+}
+
+-(nonnull RUTextViewWithPlaceholderContainerView*)genericTableViewCell_CustomView_RUTextViewWithPlaceholderContainerView_textCenteredVertically
+{
+	RUTextViewWithPlaceholderContainerView* textViewWithPlaceholderContainerView = [self genericTableViewCell_CustomView_RUTextViewWithPlaceholderContainerView];
+	[self apply_textCenteredVertically_toRUTextViewWithPlaceholderContainerView:textViewWithPlaceholderContainerView];
+	
+	return textViewWithPlaceholderContainerView;
+}
+
+-(nonnull RUTextViewWithPlaceholderContainerView*)genericTableViewCell_CustomView_RUTextViewWithPlaceholderContainerView_textCenteredHorizontallyAndVertically
+{
+	RUTextViewWithPlaceholderContainerView* textViewWithPlaceholderContainerView = [self genericTableViewCell_CustomView_RUTextViewWithPlaceholderContainerView];
+	[self apply_textCenteredHorizontally_toRUTextViewWithPlaceholderContainerView:textViewWithPlaceholderContainerView];
+	[self apply_textCenteredVertically_toRUTextViewWithPlaceholderContainerView:textViewWithPlaceholderContainerView];
+	
 	return textViewWithPlaceholderContainerView;
 }
 
